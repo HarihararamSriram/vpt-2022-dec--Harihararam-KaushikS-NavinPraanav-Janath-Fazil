74e1a7218c339497e469f671698ba6f9e993eaaf
20i230-Navin <91049629+20i230-Navin@users.noreply.github.com>
2022-12-31 14:28:17 +0530

    Completed the home page

--- vpt-frontend-app/src/components/Book.jsx
+++ vpt-frontend-app/src/components/Book.jsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import BookItem from "./BookItem";
 import "./styles/book.css";
+const LARGE = 1;
+const SMALL = 0;
 export default function Book() {
+  const [viewMode, setViewMode] = useState(1);
+  const smallClickHandler = () => {
+    console.log("Hello");
+    setViewMode(SMALL);
+  };
+  const largeClickHandler = () => {
+    setViewMode(LARGE);
+  };
   return (
     <>
       <div id="large-th">
         <div className="container">
           <div className="choose">
-            <a href="#list-th">
-              <i className="fa fa-th-list" aria-hidden="true"></i>
+            <a onClick={smallClickHandler} /* href="#list-th" */>
+              <i className="fa fa-th-list top-icons" aria-hidden="true"></i>
             </a>
-            <a href="#large-th">
-              <i className="fa fa-th-large" aria-hidden="true"></i>
+            <a onClick={largeClickHandler} /* href="#large-th" */>
+              <i className="fa fa-th-large top-icons" aria-hidden="true"></i>
             </a>
           </div>
           <div id="list-th">
-            <BookItem />
-            <BookItem />
-            <BookItem />
-            <BookItem />
+            <BookItem viewMode={viewMode} />
+            <BookItem viewMode={viewMode} />
+            <BookItem viewMode={viewMode} />
+            <BookItem viewMode={viewMode} />
           </div>
         </div>
       </div>
--- vpt-frontend-app/src/components/BookItem.jsx
+++ vpt-frontend-app/src/components/BookItem.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 import "./styles/book.css";
-export default function BookItem() {
+import ReactLogo from "../assets/bookmark.svg";
+const LARGE = 1;
+const SMALL = 0;
+export default function BookItem(props) {
+  console.log(props);
   return (
     <>
-      <div className="book read">
+      <div
+        className={"book read ".concat(
+          props.viewMode === SMALL ? "smallTiles" : "largeTiles"
+        )}
+      >
         <div className="cover">
           <img src="https://s-media-cache-ak0.pinimg.com/564x/f9/8e/2d/f98e2d661445620266c0855d418aab71.jpg" />
         </div>
-        <div className="description">
-          <p className="title">
-            Frankenstein
-            <br />
-            <span className="author">Mary Shelley</span>
-            <br />
-            <span className="desc">desciption</span>
-          </p>
+        <div>
+          <p className="title">Frankenstein</p>
+          {props.viewMode === LARGE && (
+            <p className="author">
+              <i className="fa fa-user"> </i> Mary Shelley
+            </p>
+          )}
+          {props.viewMode === SMALL && (
+            <p className="description>"> description </p>
+          )}
         </div>
         <div className="bookmark">
-          {/* <svg
-            style={"color: blue; --darkreader-inline-color:#337dff;"}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            data-darkreader-inline-color=""
-          >
-            <path
-              d="M18,2H6A1,1,0,0,0,5,3V21a1,1,0,0,0,1.65.76L12,17.27l5.29,4.44A1,1,0,0,0,18,22a.84.84,0,0,0,.38-.08A1,1,0,0,0,19,21V3A1,1,0,0,0,18,2ZM17,18.86,12.64,15.2a1,1,0,0,0-1.28,0L7,18.86V4H17Z"
-              fill="blue"
-            ></path>
-          </svg> */}
+          {props.viewMode === LARGE && <img src={ReactLogo} height={40} />}
         </div>
       </div>
     </>
